565caa879b99db60dd1dd8316db181d1fc29cdc5
sergiocascante <sergiocascante561@gmail.com>
2026-07-09 12:06:46 -0600

    Corrige estilos del encabezado del catalogo de cursos en MVC.
    - Aplica fondo celeste #C4E2F5 en section.encabezado-catalogo, h1 y parrafo descriptivo.
    
    - Agrega override al final de cursos.css para anular el background blanco global de index.css y profesores.css.
    
    - Actualiza la carga del stylesheet a css/cursos.css?v=3 para forzar recarga y evitar cache del navegador.
    
    - Mantiene consistencia visual con el navbar y logo de NextGen Academy.

--- NextGenAcademy/views/layout/header.php
+++ NextGenAcademy/views/layout/header.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="css/profesores.css">
     <link rel="stylesheet" href="css/contacto.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/cursos.css">
+    <link rel="stylesheet" href="css/cursos.css?v=3">
     <script src="js/bootstrap.bundle.min.js"></script>
 </head>
 
